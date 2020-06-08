6d8db11db7d220c67aad364b3c11fd7b14453421
Hiking <150023505@qq.com>
2020-06-08 08:49:34 +0800

    完成登陆页面以及页面跳转

--- assets/js/login.js
+++ assets/js/login.js
@@ -10,7 +10,7 @@
             })
 
             //注册功能
-            $('.login form').on('submit', function (e) {
+            $('.register form').on('submit', function (e) {
                 e.preventDefault()
                 var data = $(this).serialize()
 
@@ -19,9 +19,46 @@
                     url: 'http://www.liulongbin.top:3007/api/reguser',
                     data: data,
                     success: function (res) {
-                        alert(res.message)
+                        // alert(res.message)
+                        layer.msg(res.message);
                         if (res.status === 0) {
-                            $('.login').show().next().hide()
+                            $(this).hide().next().show()
+                        }
+                    }
+                });
+            })
+
+            // 注册表单验证
+            var form = layui.form
+
+            form.verify({
+                len: [/^[\S]{6,12}$/, '密码长度不对'],
+
+                same: function (val) {
+                    var password = $('.pass').val()
+                    if (val !== password) {
+                        return '两次密码不一致'
+                    }
+                }
+            })
+
+            //登陆 验证
+            $('.login form').on('submit', function (e) {
+                //阻止跳转
+                e.preventDefault()
+                var data = $(this).serialize()
+                $.ajax({
+                    type: "POST",
+                    url: 'http://www.liulongbin.top:3007/api/login',
+                    data: data,
+                    success: function (res) {
+                        // console.log(res.message);
+                        layer.msg(res.message);
+
+                        // alert(res.message)
+                        if (res.status === 0) {
+                            location.href = '/index.html'
+                            localStorage.setItem('token', res.token)
                         }
                     }
                 });
